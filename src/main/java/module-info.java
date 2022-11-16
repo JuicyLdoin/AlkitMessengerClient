f2b9331458a3e408af1566c381f6f4e669c7b1b4
juicyldoin <ldoinplay224@gmail.com>
2022-11-16 18:29:01 +0200

    bug fix

---
+++ src/main/java/module-info.java
@@ -0,0 +1,20 @@
+module com.example.alkitbrowser {
+
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+    requires javafx.graphics;
+
+    requires com.google.gson;
+
+    requires lombok;
+    requires org.jetbrains.annotations;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens net.alkitmessenger.client.controllers to javafx.fxml, javafx.web, lombok, com.google.gson;
+    opens net.alkitmessenger.client to javafx.fxml, javafx.graphics, javafx.web, lombok, com.google.gson;
+    opens net.alkitmessenger.util to lombok, com.google.gson;
+
+}
