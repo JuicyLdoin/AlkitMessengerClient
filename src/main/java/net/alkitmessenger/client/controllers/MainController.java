d49eaa946b09768205863b2f6bce296780d87ee8
juicyldoin <ldoinplay224@gmail.com>
2022-11-16 22:53:17 +0200

    design update

--- src/main/java/net/alkitmessenger/client/controllers/MainController.java
+++ src/main/java/net/alkitmessenger/client/controllers/MainController.java
@@ -1,13 +1,17 @@
 package net.alkitmessenger.client.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 
-public class MainController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
 
     @FXML
     private ImageView user_avatar;
@@ -23,6 +27,11 @@ public class MainController {
     private TextField friends_find_field;
     @FXML
     private TextField user_find_field;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 
     @FXML
     private void onFriendFindClick() {
