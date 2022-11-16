3990490b3d4c5de381f34132aa4527b276e13630
juicyldoin <ldoinplay224@gmail.com>
2022-11-16 18:48:47 +0200

    bug fix

--- src/main/java/net/alkitmessenger/client/AlkitMessengerClient.java
+++ src/main/java/net/alkitmessenger/client/AlkitMessengerClient.java
@@ -1,6 +1,46 @@
 package net.alkitmessenger.client;
 
-public class AlkitMessengerClient {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
 
+import java.io.IOException;
 
+@Getter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class AlkitMessengerClient extends Application {
+
+    private static AlkitMessengerClient alkitMessengerClient;
+
+    public static AlkitMessengerClient getAlkitMessengerClient() {
+
+        return alkitMessengerClient;
+
+    }
+
+    Parent main;
+    Scene scene;
+
+    public void start(Stage stage) throws IOException {
+
+        alkitMessengerClient = this;
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/scenes/mainScene.fxml"));
+        main = fxmlLoader.load();
+
+        scene = new Scene(main, 820, 480);
+
+        stage.setMaximized(true);
+
+        stage.setTitle("AlkitMessenger");
+        stage.setScene(scene);
+
+        stage.show();
+
+    }
 }
