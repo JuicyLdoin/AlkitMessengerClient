c74535c6fa46e0c7ea3cdf2105bfc73229f57676
juicyldoin <ldoinplay224@gmail.com>
2022-11-16 18:19:52 +0200

    added main stage

---
+++ src/main/java/net/alkitmessenger/client/controller/MainController.java
@@ -0,0 +1,40 @@
+package net.alkitmessenger.client.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+
+public class MainController {
+
+    @FXML
+    private ImageView user_avatar;
+    @FXML
+    private Label user_name_label;
+
+    @FXML
+    private GridPane friends_list;
+    @FXML
+    private VBox users_list;
+
+    @FXML
+    private TextField friends_find_field;
+    @FXML
+    private TextField user_find_field;
+
+    @FXML
+    private void onFriendFindClick() {
+
+
+
+    }
+
+    @FXML
+    private void onUserFindClick() {
+
+
+
+    }
+}
--- src/main/java/net/alkitmessenger/util/HashUtil.java
+++ src/main/java/net/alkitmessenger/util/HashUtil.java
@@ -12,7 +12,6 @@ public class HashUtil {
 
         try {
 
-
             MessageDigest md = MessageDigest.getInstance("SHA-512");
             md.update(str.getBytes());
 
