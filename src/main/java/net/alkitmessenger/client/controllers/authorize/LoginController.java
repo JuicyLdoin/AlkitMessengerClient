33adab16b7823404d994b8b513154bcdcbe56736
juicyldoin <ldoinplay224@gmail.com>
2022-11-19 13:55:33 +0200

    added login controller

---
+++ src/main/java/net/alkitmessenger/client/controllers/authorize/LoginController.java
@@ -0,0 +1,61 @@
+package net.alkitmessenger.client.controllers.authorize;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
+import net.alkitmessenger.client.AlkitMessengerClient;
+import net.alkitmessenger.packet.packets.output.UserLoginPacket;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Getter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class LoginController implements Initializable {
+
+    final AlkitMessengerClient alkitMessengerClient = AlkitMessengerClient.getAlkitMessengerClient();
+
+    @FXML
+    TextField mailField;
+    @FXML
+    PasswordField passwordField;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+
+
+    }
+
+    @FXML
+    private void onAuthorizeClick() {
+
+        String mail = mailField.getText();
+        String password = passwordField.getText();
+
+        Pattern mailPattern = Pattern.compile("^[\\w-\\+]+(\\.[\\w]+)*@[\\w-]+(\\.[\\w]+)*(\\.[a-z]{2,})$");
+        Matcher mailMatcher = mailPattern.matcher(mail);
+
+        if (!mailMatcher.matches())
+            return;
+
+        if (password.length() == 0)
+            return;
+
+        alkitMessengerClient.getServerConnection().addPacket(new UserLoginPacket(password));
+
+    }
+
+    @FXML
+    private void onRegistrationClick() {
+
+
+
+    }
+}
