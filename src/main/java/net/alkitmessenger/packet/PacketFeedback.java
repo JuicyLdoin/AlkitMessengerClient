633c5c19c54babf845bbff2e3ec41594b56f7b58
juicyldoin <ldoinplay224@gmail.com>
2022-11-19 18:47:41 +0200

    packet feedback fix

--- src/main/java/net/alkitmessenger/packet/PacketFeedback.java
+++ src/main/java/net/alkitmessenger/packet/PacketFeedback.java
@@ -1,13 +1,40 @@
 package net.alkitmessenger.packet;
 
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.function.Consumer;
 
-public record PacketFeedback(@NotNull Thread waitThread, @Nullable Packets packet, @Nullable String exception, @NotNull Consumer<Reason> consumer) {
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class PacketFeedback {
+
+    final Thread waitThread;
+
+    final Packets packet;
+    final String exception;
+
+    final Consumer<PacketFeedback> consumer;
+
+    Reason reason;
+
+    public PacketFeedback(@NotNull Thread waitThread, @Nullable Packets packet, @Nullable String exception, @NotNull Consumer<PacketFeedback> consumer) {
+
+        this.waitThread = waitThread;
+
+        this.packet = packet;
+        this.exception = exception;
+
+        this.consumer = consumer;
+
+    }
 
     public void resume(Reason reason) {
+
+        this.reason = reason;
 
         synchronized (waitThread) {
 
@@ -15,7 +42,7 @@ public record PacketFeedback(@NotNull Thread waitThread, @Nullable Packets packe
 
         }
 
-        consumer.accept(reason);
+        consumer.accept(this);
 
     }
 
