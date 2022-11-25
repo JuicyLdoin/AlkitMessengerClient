4d98cd3c45b8fa9e6d914da1e3a46e2db3318cbb
juicyldoin <ldoinplay224@gmail.com>
2022-11-25 23:44:58 +0200

    update PacketFeedback.java

--- src/main/java/net/alkitmessenger/packet/PacketFeedback.java
+++ src/main/java/net/alkitmessenger/packet/PacketFeedback.java
@@ -6,6 +6,7 @@ import lombok.experimental.FieldDefaults;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Map;
 import java.util.function.Consumer;
 
 @Data
@@ -17,26 +18,30 @@ public class PacketFeedback {
     final Packets packet;
     final String exception;
 
-    final Consumer<PacketFeedback> consumer;
+    final Map<Reason, Consumer<PacketFeedback>> consumerMap;
 
     Packet receivedPacket;
     Reason reason;
 
-    public PacketFeedback(@NotNull Thread waitThread, @Nullable Packets packet, @Nullable String exception, @NotNull Consumer<PacketFeedback> consumer) throws InterruptedException {
+    public PacketFeedback(@NotNull Thread waitThread, @Nullable Packets packet, @Nullable String exception, @NotNull Map<Reason, Consumer<PacketFeedback>> consumerMap) {
 
         this.waitThread = waitThread;
 
         this.packet = packet;
         this.exception = exception;
 
-        this.consumer = consumer;
+        this.consumerMap = consumerMap;
 
     }
 
     public void resume(Reason reason) {
 
         this.reason = reason;
-        consumer.accept(this);
+
+        consumerMap.keySet()
+                .stream()
+                .filter(key -> key.equals(reason))
+                .forEach(key -> consumerMap.get(key).accept(this));
 
         synchronized (this) {
 
